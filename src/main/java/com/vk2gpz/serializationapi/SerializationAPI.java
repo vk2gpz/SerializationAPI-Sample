3f066d9ba0fd30ecdbacd7dd919709bd886c12cb
vk2gpz <vk2gpz@gmail.com>
2017-06-15 12:10:23 +1000

    added SerializationAPI.

---
+++ src/main/java/com/vk2gpz/serializationapi/SerializationAPI.java
@@ -0,0 +1,29 @@
+package com.vk2gpz.serializationapi;
+
+import com.vk2gpz.serializationapi.item.ItemSerialization;
+import org.bukkit.Bukkit;
+import org.bukkit.Material;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.inventory.Inventory;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.plugin.Plugin;
+import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.scheduler.BukkitRunnable;
+
+import java.io.File;
+import java.lang.reflect.Method;
+import java.util.Map;
+import java.util.UUID;
+import java.util.logging.Logger;
+
+public class SerializationAPI extends JavaPlugin {
+	public static void saveVault(final Player player) {}
+}
+
